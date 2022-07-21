5ac1d723a8324ff51e87ce0b93b3b20c72e2560f
Carlos Miranda <miranda.bouzas@gmail.com>
2022-07-21 22:05:26 +0200

    maqquetacióon y botones de acción

--- resources/views/titulaciones/lista.blade.php
+++ resources/views/titulaciones/lista.blade.php
@@ -5,22 +5,28 @@
     <div class="row">
         <div class="col text-center">
             <h2>Titulaciones</h2>
-            <a href="{{ route('titulaciones.create') }}">Nueva</a>
+            <a href="{{ route('titulaciones.create') }}" class="link-primary">Nueva</a>
         </div>
-        <!-- <div class="col">
-            <a href="{{ route('titulaciones.create') }}">Nueva</a>
-        </div> -->
     </div>
 
     <div class="row">
-        <div class="col fw-bold">Nombre</div>
+        <div class="col-4 fw-bold">Nombre</div>
+        <div class="col fw-bold">Acciones</div>
     </div>
     @foreach ($titulaciones as $titulacion)
     <div class="row">
-        <div class="col">
-            <a href="{{ route('titulaciones.show', ['titulacion' => $titulacion]) }}">
+        <div class="col-4">
+            <a href="{{ route('titulaciones.show', ['titulacion' => $titulacion]) }}" class="link-info">
                 {{ $titulacion->nombre }}
             </a>
+        </div>
+        <div class="col d-flex">
+            <a href="{{ route('titulaciones.edit', ['titulacion' => $titulacion]) }}" class="link-secondary me-2">Actualizar</a>
+            <form action="{{ route('titulaciones.destroy', ['titulacion' => $titulacion]) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <a href="#" class="link-danger" onclick="if (confirm('Desea borrar la titulación?')){this.closest('form').submit()}">Borrar</a>
+            </form>
         </div>
     </div>
     @endforeach
--- resources/views/titulaciones/ver.blade.php
+++ resources/views/titulaciones/ver.blade.php
@@ -1,12 +1,16 @@
 <x-layout>
     <x-slot:title>Ver titulaci&oacute;n</x-slot:title>
     <div class="row">
+        <div class="col"><a href="{{ route('titulaciones.index') }}" class="link-secondary float-end">Volver al listado</a>
+        </div>
+    </div>
+    <div class="row">
         <div class="col text-center">
             <h2>Detalles de la titulación</h2>
         </div>
     </div>
     <div class="row">
-        <div class="col">
+        <div class="col-4">
             <div class="row">
                 <div class="col">
                     <span class="fw-bold">Nombre</span>: {{ $titulacion->nombre }}
@@ -23,12 +27,12 @@
                 </div>
             </div>
         </div>
-        <div class="col">
-            <a href="{{ route('titulaciones.edit', ['titulacion' => $titulacion]) }}">Actualizar</a>
+        <div class="col d-flex align-items-center">
+            <a href="{{ route('titulaciones.edit', ['titulacion' => $titulacion]) }}" class="btn btn-success me-2">Actualizar</a>
             <form action="{{ route('titulaciones.destroy', ['titulacion' => $titulacion]) }}" method="POST">
                 @csrf
                 @method('DELETE')
-                <input type="submit" class="btn btn-danger float-end" value="Borrar" onclick="return confirm('Desea borrar la titulación?')" />
+                <input type="submit" class="btn btn-danger" value="Borrar" onclick="return confirm('Desea borrar la titulación?')" />
             </form>
         </div>
     </div>
