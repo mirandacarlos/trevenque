5b5ce31859ec3180907fc648565aeed2d470030d
Carlos Miranda <miranda.bouzas@gmail.com>
2022-07-22 17:21:04 +0200

    tabla de asignaturas

---
+++ database/migrations/trevenque/2022_07_22_142006_create_asignaturas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asignaturas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->integer('creditos');
+            $table->string('curso_academico');
+            $table->integer('maximo');
+            $table->foreignId('titulacion_id')
+                ->constrained('titulaciones')
+                ->restrictOnDelete()
+                ->cascadeOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asignaturas');
+    }
+};
