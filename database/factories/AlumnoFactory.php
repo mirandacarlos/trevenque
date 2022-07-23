464bc08932158efe0bd138a8cdc168377fdec7ee
Carlos Miranda <miranda.bouzas@gmail.com>
2022-07-23 14:53:18 +0200

    tabla, modelo, factory y seeder de alumno

---
+++ database/factories/AlumnoFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use DateTime;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Alumno>
+ */
+class AlumnoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nombre' => $this->faker->firstName(),
+            'apellidos' => $this->faker->lastName(),
+            'nacimiento' => $this->faker->year('-18 years')
+        ];
+    }
+}
