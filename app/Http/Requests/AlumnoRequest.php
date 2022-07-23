820edbaa48c8aaa4feccbfdce2ed4052fd7dde77
Carlos Miranda <miranda.bouzas@gmail.com>
2022-07-23 18:24:51 +0200

    validador alumno

---
+++ app/Http/Requests/AlumnoRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AlumnoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'nombre' => 'required',
+            'apellidos' => 'required',
+            'nacimiento' => 'required|numeric'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'nombre.required' => 'El nombre es obligatorio',
+            'apellidos.required' => 'Los apellidos son obligatorios',
+            'nacimiento.required' => 'El año es obligatorio',
+            'nacimiento.numeric' => 'El año debe ser numérico'
+        ];
+    }
+}
