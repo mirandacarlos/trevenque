724caa7a9cb85b0760d23dbd990eae5ce5bda1e6
Carlos Miranda <miranda.bouzas@gmail.com>
2022-07-24 23:57:36 +0200

    validar capacidad e inscribir alumnos en asignatura

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Asignatura;
+use App\Models\Curso;
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    /**
+     * Guardar inscripción de alumnos a una asignatura.
+     * 
+     */
+    public function inscribirAlumnos(Request $request)
+    {
+        if ($request->has('alumno_id')) {
+            $asignatura = Asignatura::find($request->asignatura_id);
+            if (count($request->alumno_id) <= $asignatura->maximo - $asignatura->alumnos->count()) {
+                foreach ($request->alumno_id as $id) {
+                    Curso::create(['asignatura_id' => $request->asignatura_id, 'alumno_id' => $id]);
+                }
+                return back()->with('status', 'Alumnos inscritos en la asignatura');
+            }
+            return back()->with('error', 'Las inscripciones superan la capacidad de la asignatura');
+        }
+        return back()->with('error', 'Debe seleccionar al menos un alumno');
+    }
+}
