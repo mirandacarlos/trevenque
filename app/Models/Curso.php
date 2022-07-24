c9c0e7361f5f47faf9f588b655f140b179941015
Carlos Miranda <miranda.bouzas@gmail.com>
2022-07-24 12:27:24 +0200

    tabla y campos asignables

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -2,14 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class Curso extends Pivot
 {
+    use HasFactory;
     /**
      * Indicates if the IDs are auto-incrementing.
      *
      * @var bool
      */
     public $incrementing = true;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'cursos';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['asignatura_id', 'alumno_id'];
 }
