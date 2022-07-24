257d8160039494cdc5d5552d5fcc067bd3719afc
Carlos Miranda <miranda.bouzas@gmail.com>
2022-07-24 23:58:13 +0200

    deshabilitar boton de inscripcion formulario modal alumnos a inscribir

--- resources/views/asignaturas/ver.blade.php
+++ resources/views/asignaturas/ver.blade.php
@@ -59,6 +59,12 @@
             </div>
         </div>
         <div class="col d-flex align-items-center">
+            <button id="inscribir" @class([
+                "btn", 
+                "btn-primary", 
+                "me-2",
+                "disabled" => $asignatura->maximo <= $asignatura->alumnos->count()
+            ]) data-bs-toggle="modal" data-bs-target="#inscribirModal">Inscribir</button>
             <a href="{{ route('asignaturas.edit', ['asignatura' => $asignatura]) }}" class="btn btn-success me-2">Actualizar</a>
             <form action="{{ route('asignaturas.destroy', ['asignatura' => $asignatura]) }}" method="POST">
                 @csrf
@@ -75,5 +81,40 @@
     </div>
 
     <x-alumnos.bandeja :alumnos="$asignatura->alumnos" :acciones="false"></x-alumnos.bandeja>
+
+    <div class="modal fade" id="inscribirModal" data-bs-backdrop="static" tabindex="-1" aria-labelledby="inscribirModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Inscribir alumnos</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="inscribirForm" action="{{ route('cursos.inscribirAlumnos') }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="asignatura_id" value="{{ $asignatura->id }}">
+                        @foreach ($alumnos as $alumno)
+                        <div class="form-check">
+                            <input name="alumno_id[]" id="{{ $alumno->id }}" class="form-check-input" type="checkbox" value="{{ $alumno->id }}">
+                            <label class="form-check-label" for="{{ $alumno->id }}">
+                                {{ $alumno->nombre.' '.$alumno->apellidos }}
+                            </label>
+                        </div>
+                        @endforeach
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" onclick="enviar()">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function enviar() {
+            document.getElementById('inscribirForm').submit()
+        }
+    </script>
 
 </x-layout>
