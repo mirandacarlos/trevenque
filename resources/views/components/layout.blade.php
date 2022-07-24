c2c3cca513bb8716aaa9618359ba55ae1cd3a7cb
Carlos Miranda <miranda.bouzas@gmail.com>
2022-07-24 23:56:10 +0200

    alerta para mensaje de error

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -25,6 +25,11 @@
         {{ session('status') }}
     </div>
     @endif
+    @if (session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+    @endif
     {{ $slot }}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     @stack('scripts')
