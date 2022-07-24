b179f70f1ebe10a6a26a3e73bf6cb7691016f75e
Carlos Miranda <miranda.bouzas@gmail.com>
2022-07-24 19:10:03 +0200

    alineamiento y quitar enlace

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,21 +6,21 @@
         </div>
     </div>
     <div class="row">
-        <div class="col text-center">
+        <div class="col text-center m-3 p-3">
             <a href="{{ route('titulaciones.index') }}">Titulaciones</a>
         </div>
-        <div class="col text-center">
+        <div class="col text-center m-3 p-3">
             <a href="{{  route('asignaturas.index') }}">Asignaturas</a>
         </div>
     </div>
     <div class="row">
-        <div class="col text-center">
+        <div class="col text-center m-3 p-3">
             <a href="{{ route('alumnos.index') }}">Alumnos</a>
         </div>
-        <div class="col text-center">
+        <!-- <div class="col text-center m-3 p-3">
             <a href="{{ route('cursos.index') }}">Cursos</a>
-        </div>
-        <div class="col text-center">
+        </div> -->
+        <div class="col text-center m-3 p-3">
             <a href="">Examenes</a>
         </div>
     </div>
