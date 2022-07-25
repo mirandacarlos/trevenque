fdd826f3684e3cd45b2aa245ca8d3482b034dbd0
Carlos Miranda <miranda.bouzas@gmail.com>
2022-07-25 11:54:35 +0200

    anchos de columnas

--- resources/views/components/asignaturas/bandeja.blade.php
+++ resources/views/components/asignaturas/bandeja.blade.php
@@ -1,9 +1,9 @@
 <div class="row">
     <div class="col-2 fw-bold">Titulación</div>
-    <div class="col-3 fw-bold">Nombre</div>
+    <div class="col-2 fw-bold">Nombre</div>
     <div class="col-1 fw-bold">Matricula</div>
     <div class="col-1 fw-bold">Cr&eacute;ditos</div>
-    <div class="col-1 fw-bold">Curso</div>
+    <div class="col-2 fw-bold">Curso</div>
     <div class="col-1 fw-bold">Capacidad</div>
     <div class="col-1 fw-bold">Inscritos</div>
     @if ($acciones)
@@ -17,14 +17,14 @@
             {{ $asignatura->titulacion->nombre }}
         </a>
     </div>
-    <div class="col-3">
+    <div class="col-2">
         <a href="{{ route('asignaturas.show', ['asignatura' => $asignatura]) }}" class="link-info">
             {{ $asignatura->nombre }}
         </a>
     </div>
     <div class="col-1">{{ $asignatura->matricula }}</div>
     <div class="col-1">{{ $asignatura->creditos }}</div>
-    <div class="col-1">{{ $asignatura->curso_academico }}</div>
+    <div class="col-2">{{ $asignatura->curso_academico }}</div>
     <div class="col-1">{{ $asignatura->maximo }}</div>
     <div class="col-1">{{ $asignatura->alumnos->count() }}</div>
     @if ($acciones)
