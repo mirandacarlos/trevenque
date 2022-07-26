e1b56a38a6fb7f6613dbd5ede6ddd44e9b040c97
Carlos Miranda <miranda.bouzas@gmail.com>
2022-07-26 17:07:58 +0200

    calificar desde el detalle de alumno

--- resources/views/alumnos/ver.blade.php
+++ resources/views/alumnos/ver.blade.php
@@ -125,5 +125,8 @@
             document.getElementById('inscribirForm').submit()
         }
     </script>
+    @push('scripts')
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    @endpush
 
 </x-layout>
